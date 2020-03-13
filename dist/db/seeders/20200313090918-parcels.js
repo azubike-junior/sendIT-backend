8fe30294c4f18aa9ed75d2b29541b572c54d02ac
zubi <azu@gmail.com>
2020-03-13 12:37:28 +0100

    new

--- dist/db/seeders/20200313090918-parcels.js
+++ dist/db/seeders/20200313090918-parcels.js
@@ -4,7 +4,17 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('parcels', [{
       parcelName: 'John Doe',
-      isBetaMember: false
+      parcelName: "bed",
+      destination: "lagos state, ikorodu, no 5 loau street",
+      destination: "lagos state, iyano paja, oshodi",
+      pickupLocation: "ikorodu turning",
+      deliveredOn: "2018-11-18",
+      parcelStatus: "TRANSMITTING",
+      parcelWeight: "333",
+      placedBy: "20",
+      parcelWeightScale: "kg",
+      sentOn: "2018-11-18",
+      presentLocation: "isale oja, mowe, ajegunle"
     }], {});
   },
   down: (queryInterface, Sequelize) => {
--- server/db/seeders/20200313090918-parcels.js
+++ server/db/seeders/20200313090918-parcels.js
@@ -5,7 +5,17 @@ module.exports = {
 
     return queryInterface.bulkInsert('parcels', [{
       parcelName: 'John Doe',
-      isBetaMember: false
+      parcelName: "bed",
+      destination: "lagos state, ikorodu, no 5 loau street",
+      destination: "lagos state, iyano paja, oshodi",
+      pickupLocation: "ikorodu turning",
+      deliveredOn: "2018-11-18",
+      parcelStatus: "TRANSMITTING",
+      parcelWeight: "333",
+      placedBy: "20",
+      parcelWeightScale: "kg",
+      sentOn: "2018-11-18",
+      presentLocation: "isale oja, mowe, ajegunle"
     }], {});
   },
 
