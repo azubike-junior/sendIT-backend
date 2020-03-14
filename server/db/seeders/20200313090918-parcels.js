2667fdfb907499a31fff90526d5666013bce96e2
zubi <azu@gmail.com>
2020-03-14 19:52:59 +0100

    new

--- server/db/seeders/20200313090918-parcels.js
+++ server/db/seeders/20200313090918-parcels.js
@@ -4,28 +4,45 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('parcels', [{
-      parcelName: 'John Doe',
-      parcelName: "bed",
-      destination: "lagos state, ikorodu, no 5 loau street",
-      destination: "lagos state, iyano paja, oshodi",
-      pickupLocation: "ikorodu turning",
-      deliveredOn: "2018-11-18",
-      parcelStatus: "TRANSMITTING",
-      parcelWeight: "333",
-      placedBy: "20",
-      parcelWeightScale: "kg",
-      sentOn: "2018-11-18",
-      presentLocation: "isale oja, mowe, ajegunle"
-    }], {});
+        parcelName: "bed",
+        destination: "lagos state, ikorodu, no 5 loau street",
+        destination: "lagos state, iyano paja, oshodi",
+        pickupLocation: "ikorodu turning",
+        deliveredOn: "2018-11-18",
+        parcelStatus: "TRANSMITTING",
+        parcelWeight: "333",
+        placedBy: "20",
+        parcelWeightScale: "kg",
+        sentOn: "2018-11-18",
+        presentLocation: "isale oja, mowe, ajegunle"
+      },
+      {
+        parcelName: "car",
+        destination: "kaduna state, lawanawa",
+        pickupLocation: "shahaa street",
+        deliveredOn: "2018-11-18",
+        parcelStatus: "TRANSMITTING",
+        parcelWeight: "1000",
+        placedBy: "20",
+        parcelWeightScale: "kg",
+        sentOn: "2018-11-18",
+        presentLocation: "ananaje lawal ondo state"
+      }, {
+        parcelName: "laptop",
+        destination: "malete, kwara state",
+        pickupLocation: " ondo state, akure",
+        deliveredOn: "2018-11-18",
+        parcelStatus: "TRANSMITTING",
+        parcelWeight: "333",
+        placedBy: "20",
+        parcelWeightScale: "kg",
+        sentOn: "2018-11-18",
+        presentLocation: "isale oja, mowe, ajegunle"
+      }
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('parcels', null, {});
   }
 };
