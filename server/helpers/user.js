21d8a7539f1e38acfc9c5b4eb1bf83ed86158567
zubi <azu@gmail.com>
2020-07-10 22:04:51 +0100

    ehhn

--- server/helpers/user.js
+++ server/helpers/user.js
@@ -9,9 +9,9 @@ const {
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../configs/config.js')[env];
-console.log(config)
+console.log(config.host)
 
-export const hostUrl = config.host === 'localhost' ? local : emailBaseUrl
+export const hostUrl = config.host === 'localhost' ? local : local
 
 export const getBaseUrl = httpRequest => {
     return `${httpRequest.protocol}://${httpRequest.get('host')}`
