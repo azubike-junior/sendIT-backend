7f6cba207cacd0b89b2276ce515865b2e372c40f
zubi <azu@gmail.com>
2020-07-14 13:49:35 +0100

    added new

--- server/helpers/user.js
+++ server/helpers/user.js
@@ -1,17 +1,14 @@
 const dotenv = require('dotenv').config()
 
 const {
-    isProd,
-    NODE_ENV,
-    emailBaseUrl,
-    local
+    emailBaseUrl
 } = process.env
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../configs/config.js')[env];
 console.log(config.host)
 
-export const hostUrl = config.host === 'localhost' ? local : local
+export const hostUrl = config.host === 'localhost' ? emailBaseUrl : emailBaseUrl
 
 export const getBaseUrl = httpRequest => {
     return `${httpRequest.protocol}://${httpRequest.get('host')}`
